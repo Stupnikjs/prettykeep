0404f29d00f3c869a28bcef06cf21302a338c7f4
stupnikk <andre.lacassagne82@gmail.com>
2023-12-08 19:35:42 +0100

    ok

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import zipfile
 import os
 import shutil 
+import json
 
 '''
 with zipfile.ZipFile('takeout.zip', 'r') as zip_ref:
@@ -12,8 +13,14 @@ main_path = "unzipped/Takeout/keep"
 
 main_dir_files = os.listdir(main_path)
 
+json_files = []
 for file in main_dir_files:
     if file.endswith('.json'):
-        print(file)
+        json_files.append(file)
+
+print(len(json_files))
+
+
+# open a json file 
 
 
