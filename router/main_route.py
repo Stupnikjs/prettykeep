affe5fd2550bf66bae0c62fd7b9f3bd4fc7ddc6d
stupnikk <andre.lacassagne82@gmail.com>
2023-12-11 10:39:55 +0100

    ok

--- router/main_route.py
+++ router/main_route.py
@@ -1,6 +1,12 @@
 from server import app 
 from script import relevant
-from flask import render_template
+from flask import render_template, request
+
+def get_labels(dict):
+    return dict['labels']['name']
+
+allLabels = map(get_labels,relevant)
+
 
 
 @app.route('/')
@@ -11,4 +17,12 @@ def index():
 @app.route('/test')
 def test():
     return render_template('fiche.html', data=relevant)
+
+@app.route('/sortby/', methods=['POST'])
+def sortby():
+    param = request.get_json()
+    
+    if param['search'] not in relevant
+    filter()
+    return 
 
