61e51fbe8c64246d3ff61a9796a3ff84d17bdf6c
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2023-12-11 17:26:25 +0100

    Update

---
+++ model.py
@@ -0,0 +1,13 @@
+
+from sqlalchemy import declarative_base
+from 
+
+class Base():
+
+
+class fiche(Base):
+     def __init__(self):
+           title
+           text_content
+           label
+           
