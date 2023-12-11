bf12d046282012f80661d50a4720678ccfbd5a30
stupnikk <andre.lacassagne82@gmail.com>
2023-12-11 12:01:27 +0100

    ok

--- router/main_route.py
+++ router/main_route.py
@@ -3,9 +3,15 @@ from script import relevant
 from flask import render_template, request
 
 def get_labels(dict):
-    return dict['labels']['name']
+    return dict['labels'][0]['name']
 
-allLabels = map(get_labels,relevant)
+'''
+def has_labels(x, dict):
+    return x == get_labels(dict)
+
+all_labels = map(get_labels,relevant)
+'''
+
 
 
 
@@ -21,8 +27,15 @@ def test():
 @app.route('/sortby/', methods=['POST'])
 def sortby():
     param = request.get_json()
-    
-    if param['search'] not in relevant
-    filter()
-    return 
+    print(param)
+    print(relevant[0])
+    return relevant[0]
+    '''
+       if param['search'] not in all_labels:
+        return 'error'
+    else: 
+        sorted = filter( lambda x: has_labels(x, relevant), relevant)
+    return sorted
+    '''
+ 
 
