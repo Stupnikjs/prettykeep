ae3ac40242d9bb84796363cae8ee6dde8a47b65e
stupnikk <andre.lacassagne82@gmail.com>
2023-12-12 18:17:02 +0100

    ok

--- model.py
+++ model.py
@@ -1,29 +1,30 @@
 
-
+from sqlalchemy import String, Column, Integer
 from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
 
 
-class fiche(Base):
+class Fiche(Base):
 
     __tablename__='fiches'
 
     id = Column(Integer,primary_key=True)
-    title = Column(String(40))
-    text_content = Column(String(40))
+    title = Column(String(200))
+    text = Column(String(2000))
     created = Column(String(20))
     updated = Column(String(20))
+    labels = Column(String(30))
 
-   def to_dict():
-     return {
-     'title':self.title, 
-     'text_content' : self.text_content,
-     'created' : self.created,
-      'updated': self.updated
-      
-}
+    def to_dict(self):
+        return {
+        'title':self.title, 
+        'text' : self.text_content,
+        'created' : self.created,
+        'updated': self.updated,
+        'labels': self.labels
+        }
 
     def __str__(self):
         return self.to_dict()
--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import os 
+from datetime import datetime 
 from flask import Flask
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import Session
+from script import relevant
+from model import Fiche
 
 
 app = Flask(__name__, template_folder="static/templates")
@@ -19,10 +22,30 @@ uri = ''
 if 'DB_URI' in os.environ: 
     uri=os.environ['DB_URI']
 
+uri= "postgresql://qedivgso:GlAGhCxeJjsTKv_MoVz35OAjyxCXTeS9@tyke.db.elephantsql.com/qedivgso" 
+
 engine = create_engine(uri)
 
 Base.metadata.create_all(engine, checkfirst=True)
+# Base.metadata.drop_all(engine)
+today = datetime.now().strftime("%d-%m-%Y %H:%M")
 
-
+for fiche in relevant:
+    fiche['created'] = today
+    fiche['updated'] = None 
+    print(fiche)
+    with Session(engine) as session:
+        fiche['created'] = today
+        fiche['updated'] = None
+        newfiche=Fiche(
+            title=fiche['title'],
+            text=fiche['text'],
+            created=fiche['created'],
+            updated=fiche['updated'],
+            labels=fiche['labels']
+            )
+        session.add(newfiche)
+        session.commit()
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=port)
