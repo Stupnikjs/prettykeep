0b2357e17fe34d8542357ee0b26296140528535a
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2023-12-12 14:12:23 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os 
 from flask import Flask
+from sqlalchemy import create_engine
 
 app = Flask(__name__, template_folder="static/templates")
 
@@ -10,6 +11,14 @@ port = "5000"
 
 if port == "":
     port = os.environ.get('PORT')
+
+uri = ''
+
+if 'DB_URI' in os.environ: 
+    uri=os.environ['DB_URI']
+
+engine = create_engine(uri)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=port)
