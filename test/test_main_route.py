e0d7a592f559b39474734c9807a88f1c3af02eea
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2023-12-20 20:44:55 +0100

    Update test_main_route.py

--- test/test_main_route.py
+++ test/test_main_route.py
@@ -3,6 +3,13 @@ from app import create_app
 import requests 
 
 
+
+class BaseCase(unittest.TestCase):
+
+    def setUp(self):
+        self.base_url = "http://127.0.0.1:5000"
+
+
 class TestMainRoute(unittest.TestCase):
     def setUp(self) -> None:
         
