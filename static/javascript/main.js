54b1da167535577773dadf3aa61b5ad40e4e5141
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2023-12-22 10:56:01 +0100

    Update main.js

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,15 +1,17 @@
 
-fonction redirect(){
+function redirect(){
 window.location.replace('/fiche')
 }
+
+let text = ''
 
 const textarea = document.querySelector('.text')
 // select update button 
 
 const updateButton = document.querySelector('.updateButton')
 
+textarea.addEventListener('change', )
 
+function postUpdate(fiche){
 
-textarea.addEventListener(e => )
-
-updateButton.addEventListener( e => )
+}
