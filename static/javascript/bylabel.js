2dc7aed0da72034390d7153c5f1427da9540f611
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2023-12-26 11:34:17 +0100

    Update bylabel.js

--- static/javascript/bylabel.js
+++ static/javascript/bylabel.js
@@ -2,21 +2,17 @@
 fiches = document.querySelectorAll('.data-card')
 
 
-index = 0
 
 
-for ( let i = 0; i < fiches.length ; i++){
-        
-     index !== i ? fiches[i].classList.add('inactive'): ""
-        
-}
+
+
 
 for ( let i = 0; i < fiches.length ; i++){
         
         fiches[i].addEventListener('click', () => {
-            index += 1
-            fiches[i].classList.toggle('inactive', index !== i)
-            fiches[i].classList.toggle('active', index == i)
+      
+            fiches[i].classList.add('inactive')
+          if( i !== fiches.length - 1){ fiches[i+1].classList.remove('inactive')} else { fiches[0].classList.remove('inactive')}
         })
         
          
