f3738a8f687ebce35d0956b247cbe58e723b0ab5
stupnikk <andre.lacassagne82@gmail.com>
2023-12-28 21:29:43 +0100

    ok

--- test/test_main_route.py
+++ test/test_main_route.py
@@ -1,11 +1,12 @@
 import os
 import sys
+import json
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import unittest
 from sqlalchemy import create_engine, text
 
 from db.initdb import init_tables
-from db.query import insert_new_fiche
+from db.query import insert_new_fiche, update_fiche_query
 from sqlalchemy import create_engine
 
 from config import TestingConfig
@@ -53,9 +54,40 @@ class BaseCase(unittest.TestCase):
             # DELETE 
             conn.execute(text('TRUNCATE TABLE fiches CASCADE; '))
             conn.commit() 
-             
+    def test_update_fiche(self):
+        fiche = {
+                "title": "test", 
+                "text" : "test", 
+                "created": "test", 
+                "updated": "test",
+                "complete_start": 0,
+                "complete_end": 0,
+        }    
+        update_fiche = fiche
+        update_fiche['title'] = "modified"      
+        with self.engine.connect() as conn: 
+
+            conn.execute(text(insert_new_fiche), fiche)
+            conn.execute(text(insert_new_fiche), fiche)
+            conn.execute(text(insert_new_fiche), fiche)
             
+            conn.commit()
+            self.client.post('/updatefiche/3', json={ "fiche" : update_fiche})
+            print('Response content:', r_after.get_data(as_text=True))
 
+            # Try to retrieve JSON content
+            updated_fiche = r_after.get_json()
 
+            # Check if JSON content is None
+            if updated_fiche is not None:
+                # Assuming 'title' is the attribute you want to check
+                assert updated_fiche['fiche']['title'] == 'modified', "title should be modified"
+            else:
+                print('Error: No JSON content in the response')
+            assert r_after.get_json()['fiche'] == 'modified', ""
+            # DELETE 
+            conn.execute(text('TRUNCATE TABLE fiches CASCADE; '))
+            conn.commit() 
+            
 if __name__ == "__main__":
     unittest.main()
